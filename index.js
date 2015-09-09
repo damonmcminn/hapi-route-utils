10cb3c7c967c853ad993a10ddf498fb7d0610be5
Damon McMinn <damon.mcminn@ticketmaster.co.uk>
2015-09-09 15:28:29 +0100

    Add ES5 build

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-import flattenDeep from 'lodash.flattendeep';
+'use strict';
 
-export function Route(method, path, config) {
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i['return']) _i['return'](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError('Invalid attempt to destructure non-iterable instance'); } }; })();
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+exports.Route = Route;
+exports.SessionRoute = SessionRoute;
+exports.generateRoutes = generateRoutes;
+exports.generateSecuredRoutes = generateSecuredRoutes;
+exports.flattenRoutes = flattenRoutes;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _lodashFlattendeep = require('lodash.flattendeep');
+
+var _lodashFlattendeep2 = _interopRequireDefault(_lodashFlattendeep);
+
+function Route(method, path, config) {
 
   // returns a route object or array of route objects
 
   // could be 'GET', or ['GET', 'POST'...]
   if (method.map) {
-    return method.map(method => {
-      let [route] = Route(method, path, config[method.toLowerCase()]);
+    return method.map(function (method) {
+      var _Route = Route(method, path, config[method.toLowerCase()]);
+
+      var _Route2 = _slicedToArray(_Route, 1);
+
+      var route = _Route2[0];
+
       return route;
     });
   }
@@ -15,32 +40,34 @@ export function Route(method, path, config) {
   return [{
     method: method,
     path: path,
-    config: config,
+    config: config
   }];
-
 }
 
-export function SessionRoute(method, path, config) {
+function SessionRoute(method, path, config) {
 
-  return Route(method, path, { auth: 'session', ...config });
-
+  return Route(method, path, _extends({ auth: 'session' }, config));
 }
 
-export function generateRoutes(routes, secured) {
+function generateRoutes(routes, secured) {
 
-  let routeGenerator = secured ? SessionRoute : Route;
+  var routeGenerator = secured ? SessionRoute : Route;
 
-  return routes.map(routeTuple => {
-    let [route] = routeGenerator.apply(null, routeTuple);
+  return routes.map(function (routeTuple) {
+    var _routeGenerator$apply = routeGenerator.apply(null, routeTuple);
+
+    var _routeGenerator$apply2 = _slicedToArray(_routeGenerator$apply, 1);
+
+    var route = _routeGenerator$apply2[0];
+
     return route;
   });
-
 }
 
-export function generateSecuredRoutes(routes) {
+function generateSecuredRoutes(routes) {
   return generateRoutes(routes, true);
 }
 
-export function flattenRoutes(xs) {
-  return flattenDeep(xs);
+function flattenRoutes(xs) {
+  return (0, _lodashFlattendeep2['default'])(xs);
 }
